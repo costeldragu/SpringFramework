1fa4041069353d1f4fd6f64b63f0f502e9458d4a
DRAGU Costel <costel.dragu@endava.com>
2018-01-22 16:47:13 +0200

    Spring Framework - updated imports

--- src/main/java/com/mdc/beans/SimpleBean.java
+++ src/main/java/com/mdc/beans/SimpleBean.java
@@ -1,9 +1,8 @@
 package com.mdc.beans;
 
-import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
